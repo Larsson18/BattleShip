e1d87a1ebdaed2578cd4e15c5169cdda973cbae9
Robin <Robin@DESKTOP-0R8QSJR>
2023-08-30 10:18:28 +0200

    Add project files.

---
+++ SpelForelasning/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelForelasning
+{
+    public class Player
+    {
+        public string Name { get; set; }
+    }
+
+    public class Human : Player
+    
+    {
+        
+    }
+
+    public class Computer : Player
+
+    {
+
+    }
+}
